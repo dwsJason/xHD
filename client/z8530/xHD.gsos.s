fc16624e9e43e8df73dc1a38676e5f1f5fa7b4b6
JASON-6700K\jandersen <JGAndersen@gmail.com>
2020-10-12 17:18:31 -0400

    gsos WIP

--- client/z8530/xHD.gsos.s
+++ client/z8530/xHD.gsos.s
@@ -7,6 +7,9 @@
 *
 * Note from Jason:
 * 	Uses Merlin 1.1.10, and CADIUS
+
+*   This Merlin supports force DP Addressing via <, which is the
+*   WDC Standard
 *
 *  See GSOS / Driver Driver Reference Manual
 *
@@ -27,7 +30,22 @@ maxDRIVES	equ		2
 
 *------------------------------------------------------------------------------
 
-nullptr equ 0
+nullptr		equ		0
+
+; Block Driver ZP Defines
+
+deviceNum	equ		$0
+callNum		equ		$2
+bufferPtr	equ		$4
+requestCount equ	$8
+trasnferCount equ	$C
+blockNum	equ		$10
+blockSize	equ		$14
+fstNum		equ		$16
+volumeID	equ		$18
+cachePriority equ	$1A
+cachePointer equ	$1C
+dibPointer	equ		$20
 
 ;
 ; $$JGA TODO, fix these 
@@ -44,24 +62,19 @@ nullptr equ 0
 ;
 ;P8ErrIoErr	=		$27
 ;
-;TxtLight	=		$e00427
-;
-;
-;P8CmdQuit	=		$65
-;
-;P8Mli		=		$BF00
-;P8DevCnt	=		$BF31
-;P8DevLst	=		$BF32
-;
-;IoSccCmdB	=		$C038
-;IoSccCmdA	=		$C039
-;IoSccDataB	=		$C03A
 
-;IoSccDataA	=		$C03B
 ;
-;IoRomIn		=		$C081
+; Status Indicator on the Text Page
 ;
-;RomStrOut	=		$DB3A		;YA=C string
+TxtLight	=		$e00427
+;
+; Serial Port Addresses
+;
+IoSccCmdB	=		$C038
+IoSccCmdA	=		$C039
+IoSccDataB	=		$C03A
+IoSccDataA	=		$C03B
+
 
 *------------------------------------------------------------------------------
 
@@ -153,10 +166,126 @@ Driver_Flush mx %00
 			clc
 			rtl
 
+*------------------------------------------------------------------------------
+;
+; Possible Error Codes
+; $11 Invalid device number
+; $23 Device not open
+; $2C Invalid byte count
+; $2D Invalid block number
+; $53 Parameter out of range
+; $58 Not a block device
+;
+
 Driver_Read mx %00
+
+;sBlockNum   = 3
+;sBlockCount = 1
+
+			lda		<blockNum		  ; current block number on the stack
+			sta		|HdrBlk			  ; packet header, block #		
+
+			lda		<requestCount+2
+			lsr
+			tax					      ; number of blocks to transfer
+
+			sep #$20
+			mx  %10
+
+			lda		<deviceNum
+			asl
+			inc		; 3=drive 1, 5=drive2
+			sta		|CurCmd
+
+			lda		<bufferPtr+2
+			pha
+			plb						  ; Current DB is Target for Data
+
+			ldy		<bufferPtr		  ; Y is Address to copy the data into
+
+]readloop	
+		    phx						  ; preserve number of blocks to xfer
+			jsr		ReadBlock
+
+			plx
+			dex
+			bne		]readloop
+
+			rep		#$31
+			mx		%00
+
+			stz		<transferCount
+			lda		<requestCount+2
+			sta		<transferCount+2
+
+			; c = 0
+			lda		#0
+			rtl
+
+
+*------------------------------------------------------------------------------
+
 Driver_Write mx %00
+			lda		#1
+			sec
+			rtl
+
+*------------------------------------------------------------------------------
+;
+; Error Codes
+; $11 Invalid device number
+; $53 Parameter out of range
+;
 Driver_Status mx %00
+
+			lda		<statusCode
+			cmp		#5
+			bcs		:errorRange
+
+			asl
+			tax
+
+			jmp		(:dispatch,x)
+
+:errorRange
+			; c = 1
+			lda		#$21	; drvrBadCode
+			rtl
+
+:dispatch	da		:GetDeviceStatus
+			da		:GetConfigParameters
+			da		:GetWaitStatus
+			da		:GetFormatOptions
+			da		:GetPartitionMap
+
+:GetDeviceStatus
+
+:GetConfigParameters
+:GetWaitStatus
+			lda		#2
+			sta		<transferCount
+			stz		<transferCount+2
+
+			lda		#0     				; GetConfigParameters
+			sta		[statusListPtr]  	; GetWaitStatus
+			; a = 0
+			; c = 0
+			rtl
+
+
+
+:GetFormatOptions
+:GetParitionMap
+			stz		<transferCount
+			stz		<transferCount+2
+			lda		#0
+			rtl
+
+
 Driver_Control mx %00
+			lda		#1
+			sec
+			rtl
 
 
 CmdHdr		;asc		"E"
@@ -164,52 +293,33 @@ CurCmd		db		0
 HdrBlk		dw		0
 HdrChecksum	db		0
 HdrCopy		ds		4-1
-;TempDate	ds		4
 HdrCopyCksum dw		1
 			
-*-------------------------------------------------
-
-E0Driver
-			jmp		(GSCmd,x)
-GSCmd
-			dw		xHdClient
-
-xHdClient
-			lda		#2
-			bit		ZpDrvrUnit
-			bpl		:GotZpDrvrUnit
-			asl
-:GotZpDrvrUnit
-			sta		CurCmd			;2=Drive1, 4=Drive2
+*------------------------------------------------------------------------------
+; Initialize the Serialport HW
+;
+; D = $C000
+; Preserves Y
+; B unknown
+;
+InitSCC		mx		%10
+;			sta		CurCmd			;2=Drive1, 4=Drive2
 
 			lda		#15				;Are 8530 interrupts disabled?
-			sta		IoSccCmdB
-			lda		IoSccCmdB
-			beq		:ConfigOK		;If not then SCC is set up by firmware, so reset it
-
-			bit		$c030
+			sta		<IoSccCmdB
+			lda		<IoSccCmdB
+			beq		:ConfigOK		;If not then SCC is set up by firmware,
+									; so reset it
+			bit		<$c030
 			ldx		#-SccInitLen
 :InitSCC	
-			ldal	SccInitTblEnd-$100,x
-			sta		IoSccCmdB
+			lda		>SccInitTblEnd-$100,x
+			sta		<IoSccCmdB
 			inx
 			bne		:InitSCC
 
 :ConfigOK
-			lda		ZpDrvrCmd
-			beq		:DoStatus		;0=status
-			dec
-			beq		ReadBlock		;1=read block
-			dec
-			bne		:NoCmd			;2=write block
-			jmp		WriteBlock
-:DoStatus
-			ldx		#$ff			;TODO - returns 32MB HD regardless of image size
-			txy
-:NoCmd
-			lda		#0				; no error
-			clc
-			rtl
+			rts
 
 ;230k baud
 SccInitTbl
@@ -224,40 +334,74 @@ SccInitTbl
 SccInitTblEnd
 SccInitLen	=		SccInitTblEnd-SccInitTbl
 
+
+;			lda		ZpDrvrCmd
+;			beq		:DoStatus		;0=status
+;			dec
+;			beq		ReadBlock		;1=read block
+;			dec
+;			bne		:NoCmd			;2=write block
+;			jmp		WriteBlock
+;:DoStatus
+;			ldx		#$ff			;TODO - returns 32MB HD regardless of image size
+;			txy
+;:NoCmd
+;			lda		#0				; no error
+;			clc
+;			rtl
+
+
 *-------------------------------------------------
-			mx		%11
+; DB = Target Block Address Bank
+;  Y = Pointer to Target Block Address
+; CurCmd, already 3 = write drive 1, or 5 = write drive 2
+; DP = need to preserve
+; HdrBlk, already has the 16 bit block number
+;
+			mx		%10
 ReadBlock
-			rep		#$10
-			xba						;ah=0
-			lda		CurCmd
+			phd						; preserve DP
+			pea		#$C000
+			pld			            ; DP on IO
+
+			jsr		InitSCC
+
+			; Legacy Support for the TxtLight
+			; $$JGA Todo (we could have a "light" on the menu bar)
+			lda		>CurCmd
 			lsr
 			ora		#$30
-			stal	TxtLight
-			inc		CurCmd			;3=drive1, 5=drive2
-			ldx		ZpDrvrBlk
-			stx		HdrBlk
-			jsr		ClearRx
-			jsr		SendCmd
-			stz		ZpChecksum
+			sta		>TxtLight
 
-		do	1
+			;ldx		ZpDrvrBlk
+			;stx		HdrBlk
+			jsr		ClearRx
+
+			phy
+			jsr		SendCmd
+			ply
+
+			lda		#0		; ZpChecksum
+
 			ldy		#HdrCopy
 			ldx		#HdrCopy+5-1
-;			ldx		#HdrCopy+9
-			stx		ZpReadEnd
+			;stx		ZpReadEnd
+			;phk
+			;plb
+
 			jsr		ReadBytes
 			bcc		ReadError
-			lda		ZpChecksum
+
+			cmp		#0		; ZpChecksum
 			bne		ReadError
 			
-			ldy		#2
-:ErrChk		lda		CmdHdr,y
-			cmp		HdrCopy,y
+			ldx		#2
+:ErrChk		lda		>CmdHdr,x
+			cmp		>HdrCopy,x
 			bne		ReadError
-			dey
+			dex
 			bpl		:ErrChk
 			
-		fin
 
 			rep		#$21
 			ldy		ZpDrvrBufPtr
@@ -271,24 +415,28 @@ ReadBlock
 
 			jsr		ReadOneByte		;Read checksum
 			bcc		ReadError		;Err if P8Timeout
-			lda		ZpChecksum		;Chksum==0?
+			cmp		#0				;Chksum==0?
 			bne		ReadError		;Err if bad chksum
 
-			tsb		TxtLight
-			sep		#$10
+			ora		>TxtLight
+			sta		>TxtLight
+
 			clc
-			rtl
+			pld
+			rts
 
 *-------------------------------------------------
 			mx		%10
 ReadError
 			jsr		ClearRx
-			lda		#0
-			tsb		TxtLight
-			sep		#$10
+
+			;lda		#0
+			;tsb		TxtLight
+
 			lda		#P8ErrIoErr
 			sec
-			rtl
+			pld
+			rts
 
 
 *-------------------------------------------------
@@ -356,74 +504,116 @@ WriteError
 *-------------------------------------------------
 			mx		%10
 SendCmd
-			inc		$c034
-			stz		ZpChecksum
-		
+			phb
+
+			inc		<$c034 			; Border Color
+
+			lda		#^CmdHdr
+			pha
+			plb
+
 			ldy		#CmdHdr
-			ldx		#HdrChecksum
-			stx		ZpReadEnd
+			ldx		#HdrChecksum	; X = End Address
+
+			lda		#0   			; Zero out checksum
 			jsr		WriteBytes
-			sta		HdrChecksum
-			dec		$c034
-			;Fall through to send checksum byte
+			sta		|HdrChecksum
+
+			jsr		WriteBytes
+
+			lda		<$c034
+			dec
+			and		#$0F
+			sta		<$c034  		; Border Color Back
+
+			plb
+			rts
 			
 *-------------------------------------------------
+;  Checksum in A
+;  B in source Bank
+;  Y index to source data
+;
 			mx		%10
 WriteBytes
-			tsx						;Init timeout
+			phx						;End Read
+			pha						;Checksum
+:WriteByte
+			ldx		#$0				;Init timeout
 			clc
 :Loop
 			inx						;P8Timeout++
 			bmi		:Exit
-			lda		IoSccCmdB		;Reg 0
+			lda		<IoSccCmdB		;Reg 0
 			and		#%00100100		;Chk bit 5 (ready to send) & bit 2 (HW handshake)
 			eor		#%00100100
 			bne		:Loop
 
-			lda		0,y				;Get byte
-			sta		IoSccDataB		;Tx byte
+			lda		|0,y			;Get byte
+			sta		<IoSccDataB		;Tx byte
 
-			eor		ZpChecksum
-			sta		ZpChecksum		;Update cksum
+			eor		1,s				; ZpChecksum
+			sta		1,s				; ZpChecksum ;Update cksum
 			
-			iny
-			cpy		ZpReadEnd
-			bcc		WriteBytes
-
+			iny						; write index
+			cpy		2,s				; EndRead
+			bcc		:WriteByte
+:Exit
+			pla						; checksum in A
+			plx
 			rts
 			
 *-------------------------------------------------
+*  Return checksum in A
+*  Return Byte in X
+* 
 			mx		%10
 ReadOneByte
-			ldy		#$C07f
-			sty		ZpReadEnd
-			;fall through to ReadBytes
+			pha						;CheckSum
+			ldx		#0				;Init Timeout
+:Loop
+			inx
+			bmi		:Exit
+
+			lda		<IoSccCmdB		;Chk reg 0 bit 0
+			lsr
+			bcc		:Loop
+
+			lda		<IoSccDataB		;Byte received
+			tax 	   				;save result to return
+			eor		1,s				;checksum
+			sta		1,s				;update checksum
+:Exit
+			pla
+			rts
 
 *-------------------------------------------------
 			mx		%10
 ReadBytes
+			phx						;EndRead
+			pha						;Checksum
 :ReadByte
-			tsx						;Init timeout
+			ldx		#0				;Init timeout
 :Loop
 			inx						;P8Timeout++
 			bmi		:Exit
-			lda		IoSccCmdB		;Chk reg 0 bit 0
+			lda		<IoSccCmdB		;Chk reg 0 bit 0
 			lsr
 			bcc		:Loop
 
-			lda		IoSccDataB		;Byte received
-			sta		0,y				;Store it
-			tax						;Save in case this is a 1 byte read
+			lda		<IoSccDataB		;Byte received
+			sta		|0,y			;Store it
 			
-			eor		ZpChecksum
-			sta		ZpChecksum		;Update cksum
+			eor		1,s				;<ZpChecksum
+			sta		1,s				;<ZpChecksum ;Update cksum
 			
 			iny
-			cpy		ZpReadEnd
+			cpy		2,s				;ZpReadEnd
 			bcc		:ReadByte
-			
-			txa						;Return last byte read
-:Exit		rts
+:Exit		
+			pla 					; checksum
+			plx 					; EndRead
+			rts
 
 *-------------------------------------------------
 			mx		%10
@@ -431,22 +621,20 @@ ClearRx
 
 :ClearFifo	
 			lda		#1
-			bit		IoSccCmdB		;Chk reg 0 bit 0
+			bit		<IoSccCmdB		;Chk reg 0 bit 0
 			beq		:Done
 
-			sta		IoSccCmdB		;Read reg 1
+			sta		<IoSccCmdB		;Read reg 1
 			lda		#$30			;Chk & Clear overrun
-			bit		IoSccCmdB		;Chk bit 5 for RX OVERRUN
+			bit		<IoSccCmdB		;Chk bit 5 for RX OVERRUN
 			beq		:NotOverrun
-			sta		IoSccCmdB
-			stz		IoSccCmdB
+			sta		<IoSccCmdB
+			stz		<IoSccCmdB
 :NotOverrun
-			lda		IoSccDataB		;Byte received
+			lda		<IoSccDataB		;Byte received
 			bra		:ClearFifo
 :Done
 			rts
-
-E0DriverEnd
 
 *-------------------------------------------------
 
